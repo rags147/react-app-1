8270226e619d8803ca2c74ad3fcd32c7a7d42e64
Anurag <rags147@gmail.com>
2023-12-31 01:14:47 +0530

    Base code for first react app

---
+++ src/Workspace.js
@@ -0,0 +1,11 @@
+function Workspace() {
+  return (
+    <div className="Worksapce">
+      User name: <input type="text" size={20}/>
+      <br/>
+      Password: <input type="password" size={20}/>
+    </div>
+  );
+}
+
+export default Workspace;
---
+++ src/menu.js
@@ -0,0 +1,6 @@
+function menu(){
+    return (
+        <div >Hello World!</div>
+    );
+}
+export default menu;
